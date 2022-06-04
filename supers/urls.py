6901da85d7a6a01d59749795a7aa5f011abf59cd
Josmar Diaz <jdm.begreat28@gmail.com>
2022-06-04 17:36:11 -0400

    Updated Admin and URLS FIle

--- supers/urls.py
+++ supers/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path 
 from . import views
 
-urlspatterns = [
-    path('heroes-villains/', views.super_types)
+urlpatterns = [
+    path('', views.super_list),
+    path('<ink:pk>/', views.super_detail),
 ]
