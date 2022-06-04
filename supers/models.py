1ff2607fbd3340845bc90b61b92662c9c6e1e78f
Josmar Diaz <jdm.begreat28@gmail.com>
2022-06-04 18:41:57 -0400

    Added Admin in Super Types Apps + Updated Models and Serializers in Supers App

--- supers/models.py
+++ supers/models.py
@@ -8,5 +8,5 @@ class Super(models.Model):
     primary_ability = models.CharField(max_length=255)
     secondary_ability = models.CharField(max_length=255)
     catchphrase = models.CharField(max_length=255)
-    super_type = models.ForeignKey(SuperType, on_delete=models.CASCADE)
+    super_type = models.ForeignKey(SuperType, on_delete=models.CASCADE, null=True)
     
